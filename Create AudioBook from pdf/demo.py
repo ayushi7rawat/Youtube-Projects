b7d71bf753082edad23d76d0f521ca04591280e9
ayushi7rawat <ayushi7rawat@gmail.com>
2020-10-04 13:07:51 +0530

    create audio book from python

---
+++ Create AudioBook from pdf/demo.py
@@ -0,0 +1,18 @@
+import pyttsx3
+import PyPDF2
+
+book = open('demo.pdf','rb')
+pdf_reader = PyPDF2.PdfFileReader(book)
+num_pages = pdf_reader.numPages
+
+play = pyttsx3.init()
+print('Playing Audio Book')
+
+for num in range(0, num_pages):
+	page = pdf_reader.getPage(num)
+
+	data = page.extractText()
+
+	play.say(data)
+
+	play.runAndWait()
---
+++ Screenshot taker app with python/Part-one.py
@@ -0,0 +1,8 @@
+'''
+Take Screenshot using python with just 3 lines of code.
+Author: Ayushi Rawat
+'''
+import pyautogui
+
+myScreenshot = pyautogui.screenshot()
+myScreenshot.save(r'C:\Users\imar\OneDrive\Desktop\screenshot.png')
