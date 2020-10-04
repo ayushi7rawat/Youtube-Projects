dc307bf9a8ceaec2a9867cedd6ea0b7eeed28339
ayushi7rawat <ayushi7rawat@gmail.com>
2020-10-04 13:10:28 +0530

    create audio book from python

--- Create AudioBook from pdf/demo.py
+++ Create AudioBook from pdf/demo.py
@@ -1,3 +1,7 @@
+'''
+Create AudioBook from any PDF using Python
+Author: Ayushi Rawat
+'''
 import pyttsx3
 import PyPDF2
 
@@ -10,9 +14,7 @@ print('Playing Audio Book')
 
 for num in range(0, num_pages):
 	page = pdf_reader.getPage(num)
-
 	data = page.extractText()
 
 	play.say(data)
-
 	play.runAndWait()
