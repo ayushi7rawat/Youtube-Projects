8ccc0a13612e053f46bb93d6b83a777531083931
ayushi7rawat <ayushi7rawat@gmail.com>
2020-10-28 09:13:40 +0530

    URL Shortener

---
+++ URL Shortner/shortner.py
@@ -0,0 +1,16 @@
+'''
+Create a URL Shortner in Python
+Author: Ayushi Rawat
+'''
+
+import pyshorteners as sh
+
+link = 'https://www.youtube.com/ayushirawat/videos'
+
+s = sh.Shortener()
+x = (s.tinyurl.short(link))
+
+print(x))
+
+# One-liner
+#print(pyshorteners.Shortener().tinyurl.short('https://www.youtube.com/ayushirawat/videos'))
