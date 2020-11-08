ecb1639d0e0f6978863093dc402c0c383a238a07
ayushi7rawat <ayushi7rawat@gmail.com>
2020-11-08 08:52:56 +0530

    jokes generator python

--- Jokes Generator/script.py
+++ Jokes Generator/script.py
@@ -8,11 +8,19 @@ Author - Ayushi Rawat
 import pyjokes
 
 #fetch the joke
-joke1 = pyjokes.get_joke(language='en', category= 'all')  
+joke1 = pyjokes.get_joke(language='en', category='all')
 
 #display the joke
 print(joke1)
 
 #a different category
-joke2 = pyjokes.get_joke(language='en', category= 'neutral')
+joke2 = pyjokes.get_joke(language='en', category='neutral')
+
+#display the joke
 print(joke2)
+
+#fetch a bunch of jokes
+jokes = pyjokes.get_jokes(language='en', category='neutral')
+
+for i in range(5):
+    print(i+1,".",jokes[i])
